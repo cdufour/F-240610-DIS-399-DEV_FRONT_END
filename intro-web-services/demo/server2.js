11c3570e2d9de53542cd1f3170a37e09c27e57e6
cdufour <opusidea@gmail.com>
2024-07-11 09:08:10 +0200

    intro ws j2 start

--- intro-web-services/demo/server2.js
+++ intro-web-services/demo/server2.js
@@ -3,14 +3,11 @@ const fs = require('fs'); // build-in module
 const soap = require('soap');
 
 // Define the service implementation
-const service = {
-  DemoService: {
-    DemoPort: {
-      post_message: function(args, callback) {
-
-        // Return the response
-        const result = { returns_resultcode: 200 };
-        callback(null, result);
+var myService = {
+  MyService: {
+    MyPort: {
+      MyFunction: function (args) {
+        return {result: args.name};
       }
     }
   }
@@ -21,12 +18,16 @@ const xml = fs.readFileSync('demo.wsdl', 'utf8');
 
 //http server example
 var server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
     res.end("404: Not Found: " + req.url);
 });
 
 const port = 8000;
 server.listen(port);
 
-soap.listen(server, '/demo', service, xml, () => {
+soap.listen(server, '/demo', myService, xml, () => {
     console.log('SOAP server running at http://localhost:'+ port +'/demo?wsdl');
 });
